783985e1134220f5967303495313a00d654bb863
Unknown <arijnafi@gmail.com>
2018-05-26 00:20:43 +0700

    demo final

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,151 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="{{ URL::asset('adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ URL::asset('adminlte/bower_components/font-awesome/css/font-awesome.min.css') }}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="{{ URL::asset('adminlte/bower_components/Ionicons/css/ionicons.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ URL::asset('adminlte/dist/css/AdminLTE.min.css') }}">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+        folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="{{ URL::asset('adminlte/dist/css/skins/_all-skins.min.css') }}">
+    <!-- Morris chart -->
+    <link rel="stylesheet" href="{{ URL::asset('adminlte/bower_components/morris.js/morris.css') }}">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="{{ URL::asset('adminlte/bower_components/jvectormap/jquery-jvectormap.css') }}">
+    <!-- Date Picker -->
+    <link rel="stylesheet" href="{{ URL::asset('adminlte/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{ URL::asset('adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.css') }}">
+    <!-- bootstrap wysihtml5 - text editor -->
+    <link rel="stylesheet" href="{{ URL::asset('adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
     <title>@yield('title')</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
-    <link rel="stylesheet" type="text/css" href="/css/formvalidation.css">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/formvalidation.css') }}">
     @yield('style')
 </head>
-<body class="body">
-    Hi, {{ $user }}!
-    <div class="row">
-        <div class="content col-md-4" style="text-align: left; background-color: white;">
-            <div class="cont" style="">
-                <a href="/lihatpermohonan">
-                    <div class="pilih">
-                        <h4>Lihat Permohonan Surat</h4>
-                    </div>
-                </a>
-                <a href="/keperluansurat">
-                    <div class="pilih">
-                        <h4>Buat Permohonan Surat</h4>
-                    </div>
-                </a>
-            </div>
+
+<body class="hold-transition skin-red sidebar-mini">
+  <div class="wrapper">
+    <!-- HEADER -->
+    <header class="main-header">
+        <!-- Logo -->
+        <a href="index.html" class="logo">
+            <!-- mini logo for sidebar mini 50x50 pixels -->
+            <span class="logo-mini"><b>S</b>I</span>
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg"><b>SITU</b>Informatika</span>
+        </a>
+        <!-- Header Navbar -->
+        <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>  
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">
+            <!-- User Account -->
+            <li class="dropdown user user-menu">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <span>{{ $user }}</span>
+                <i class="glyphicon glyphicon-menu-down" style="margin: 0 0 0 5px"></i>
+              </a>
+              <ul class="dropdown-menu">
+                <li class="user-footer">
+                  <a href="/logout">Keluar</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
-        <div id="container" class="col-md-8">
-            @yield('content')
+      </nav>
+    </header>
+
+    <!-- SIDEBAR -->
+    <aside class="main-sidebar">
+      <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel" style="margin: 20px 0 50px 0">
         </div>
+        <!-- sidebar menu -->
+        <ul class="sidebar-menu">
+          <li class="treeview">
+            <a href="/lihatpermohonan">
+              <i class="fa fa-folder"></i> <span>Lihat Permohonan Surat</span>
+            </a>
+          </li>
+          <li class="treeview">
+            <a href="/keperluansurat">
+              <i class="glyphicon glyphicon-cloud-upload"></i> <span>Buat Permohonan Surat</span>
+            </a>
+          </li>
+        </ul>
+      </section>
+    </aside>
+
+    <!-- CONTENT WRAPPER -->
+    <div class="content-wrapper">
+        @yield('content')
     </div>
-    <a href="/logout">LOG OUT</a>
+  
+    <!-- FOOTER -->
+    <footer class="main-footer">
+      <strong>Copyright &copy; 2018 <a href="https://adminlte.io">HAFARARIJ</a>.</strong>
+    </footer>
+  </div>
+
+  <!-- SCRIPT -->
+  <!-- jQuery 3 -->
+  <script src="adminlte/bower_components/jquery/dist/jquery.min.js"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="adminlte/bower_components/jquery-ui/jquery-ui.min.js"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
+        $.widget.bridge('uibutton', $.ui.button);
+  </script>
+  <!-- Bootstrap 3.3.7 -->
+  <script src="adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- Morris.js charts -->
+  <script src="adminlte/bower_components/raphael/raphael.min.js"></script>
+  <script src="adminlte/bower_components/morris.js/morris.min.js"></script>
+  <!-- Sparkline -->
+  <script src="adminlte/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+  <!-- jvectormap -->
+  <script src="adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+  <script src="adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+  <!-- jQuery Knob Chart -->
+  <script src="adminlte/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+  <!-- daterangepicker -->
+  <script src="adminlte/bower_components/moment/min/moment.min.js"></script>
+  <script src="adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+  <!-- datepicker -->
+  <script src="adminlte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+  <!-- Bootstrap WYSIHTML5 -->
+  <script src="adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+  <!-- Slimscroll -->
+  <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+  <!-- FastClick -->
+  <script src="adminlte/bower_components/fastclick/lib/fastclick.js"></script>
+  <!-- AdminLTE App -->
+  <script src="adminlte/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="adminlte/dist/js/pages/dashboard.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="adminlte/dist/js/demo.js"></script> 
 </body>
 </html>
